7eed95943c185fc2ccad2923d9f7b21826110132
Joe Baldwin <contact@humboldttechgroup.com>
2014-04-28 20:34:57 -0500

    synch

---
+++ 04_28_14/SuperHeroSharedStore Final/SuperHeroSharedStore/Hero.m
@@ -0,0 +1,21 @@
+//
+//  Hero.m
+//  SuperHeroSharedStore
+//
+//  Created by student on 4/28/14.
+//  Copyright (c) 2014 fvtc. All rights reserved.
+//
+
+#import "Hero.h"
+
+
+@implementation Hero
+
+@dynamic age;
+@dynamic heroId;
+@dynamic image;
+@dynamic name;
+@dynamic power;
+@dynamic secret;
+
+@end
